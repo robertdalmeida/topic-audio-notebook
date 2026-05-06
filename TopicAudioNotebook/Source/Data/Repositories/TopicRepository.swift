c5cc88c301834b2072a450239a14ca55df31150d
robertd <robert.dalmeida@adyen.com>
2026-05-06 23:55:21 +0200

    Refactor project structure to feature-based architecture with Domain/Data/UI separation
    - Reorganize Source/ into Application/, Domain/, Data/, Features/, and UI/ directories
    - Move TopicAudioNotebookApp to Application/ folder
    - Group Models into Domain/Models/ (Topic, Recording, Note)
    - Extract protocols to Domain/Protocols/ (StorageProvider, SummarizationService)
    - Extract TopicRepositoryProtocol to Domain/Repositories/ from TopicRepository implementation
    - Move Services/ to Data/Services/ with

--- TopicAudioNotebook/Source/Data/Repositories/TopicRepository.swift
+++ TopicAudioNotebook/Source/Data/Repositories/TopicRepository.swift
@@ -1,40 +1,6 @@
 import Foundation
 import Combine
 
-protocol TopicRepositoryProtocol: AnyObject {
-    var topics: [Topic] { get }
-    var topicsPublisher: AnyPublisher<[Topic], Never> { get }
-    var isLoading: Bool { get }
-    var errorMessage: String? { get }
-    
-    func addTopic(name: String, description: String, color: TopicColor)
-    func updateTopic(_ topic: Topic)
-    func deleteTopic(_ topic: Topic)
-    
-    func addRecording(to topicId: UUID, title: String, fileURL: URL, duration: TimeInterval)
-    func deleteRecording(_ recording: Recording, from topicId: UUID)
-    
-    func addNote(to topicId: UUID, content: String)
-    func updateNote(_ note: Note, in topicId: UUID)
-    func deleteNote(_ note: Note, from topicId: UUID)
-    
-    func archiveRecording(_ recording: Recording, in topicId: UUID)
-    func unarchiveRecording(_ recording: Recording, in topicId: UUID)
-    func archiveNote(_ note: Note, in topicId: UUID)
-    func unarchiveNote(_ note: Note, in topicId: UUID)
-    
-    func updateRecordingTranscript(recordingId: UUID, in topicId: UUID, transcript: String)
-    func transcribeRecording(recordingId: UUID, in topicId: UUID) async
-    func retryTranscription(for recording: Recording, in topicId: UUID)
-    
-    func generateRecordingSummary(recordingId: UUID, in topicId: UUID) async
-    func consolidateSummary(for topicId: UUID) async
-    
-    func getRecordingsDirectory() -> URL
-    func switchStorage(to type: StorageType) async
-    var currentStorageType: StorageType { get }
-}
-
 @MainActor
 final class TopicRepository: ObservableObject, TopicRepositoryProtocol {
     @Published private(set) var topics: [Topic] = []
